4ee3062e9dcb6714b958032f75a8a9352ada13c8
luizapmentel <luiza.pimentel.lima@gmail.com>
2025-11-16 22:22:13 -0300

    adicionando construtor, getters e setters na classe tutor

--- src/Tutor.java
+++ src/Tutor.java
@@ -8,4 +8,50 @@ public class Tutor {
 		this.telefone = telefone;
 		this.dataCadastro = dataCadastro;
 	}
+
+	// Setter de CPF omitido pois é identificador único, não podendo ser alterado.
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(String dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    @Override 
+	public String toString() {
+	    return "Tutor: " + nome + 
+	           " | CPF: " + cpf + 
+	           " | E-mail: " + email + 
+	           " | Telefone: " + telefone + 
+	           " | Data Cadastro: " + dataCadastro;
+	}
 }
