a88ccbaef6634f1309f3daccd72b761b72590126
luizapmentel <luiza.pimentel.lima@gmail.com>
2025-11-16 21:54:10 -0300

    primeiro commit

---
+++ src/Tutor.java
@@ -0,0 +1,11 @@
+public class Tutor {
+	private String nome, cpf, email, telefone, dataCadastro;
+	
+	public Tutor(String nome, String cpf, String email, String telefone, String dataCadastro) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.telefone = telefone;
+		this.dataCadastro = dataCadastro;
+	}
+}
